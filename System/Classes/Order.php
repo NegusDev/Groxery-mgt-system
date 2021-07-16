8783d84d6e27b552d5bf48a1998f39e3eb500c06
negusDev <enochnegus@gmail.com>
2021-07-16 17:44:35 +0300

    Update Order.php

--- System/Classes/Order.php
+++ System/Classes/Order.php
@@ -5,7 +5,7 @@ class Order extends DbController
 
     public function getAllOrders()
     {
-        $sql = "SELECT * FROM orders ORDER BY date";
+        $sql = "SELECT * FROM orders ORDER BY datetime DESC";
         $result = $this->conn->query($sql) or die($this->conn->error);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
